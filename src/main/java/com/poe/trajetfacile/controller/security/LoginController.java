1d1c68177216e7ceef30eea8abce6739dbd70b9f
jérémy Réchet <jeremyr@hotmail.fr>
2018-02-08 14:18:27 +0100

    erreur websocket après login

--- src/main/java/com/poe/trajetfacile/controller/security/LoginController.java
+++ src/main/java/com/poe/trajetfacile/controller/security/LoginController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-    // Login form
+
+
     @RequestMapping("/login")
     public String login() {
         return "login";
